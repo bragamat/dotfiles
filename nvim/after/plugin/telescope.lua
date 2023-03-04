778d2b6db94bb72d8bb7ab752f48de2dc82d8285
bragamat <mateuscbraga@gmail.com>
2023-03-04 00:30:04 -0300

    feat: added ag

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,5 +3,6 @@ vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-f>', builtin.git_files, {})
 vim.keymap.set('v', '<C-p>', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.load_extension("ag")
 end)
 
--- nvim/lua/bragamat/packer.lua
+++ nvim/lua/bragamat/packer.lua
@@ -29,6 +29,7 @@ return require('packer').startup(function(use)
 		-- or                            , branch = '0.1.x',
 		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
+    use({ "kelly-lin/telescope-ag", requires = { { "nvim-telescope/telescope.nvim" } } })
 	use({
 		'rose-pine/neovim',
 		as = 'rose-pine',
